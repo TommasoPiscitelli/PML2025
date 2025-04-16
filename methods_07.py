38f5712706d5bff4e25a243c34651054fef3fb84
r-doz <romina.ma95@gmail.com>
2025-04-16 10:51:03 +0200

    Notebook 7 solved

--- methods_07.py
+++ methods_07.py
@@ -7,10 +7,18 @@ def conditioned_scale_file(obs, guess=8.5):
     return measurement
 
 def eight_school_file(J, sigma, y=None):
-    #your code here
-    return
+    mu = pyro.sample("mu", dist.Normal(0., 5.))
+    tau = pyro.sample("tau", dist.HalfCauchy(5.))
+    with pyro.plate("schools", J):
+        theta = pyro.sample("theta", dist.Normal(mu, tau))
+        obs = pyro.sample("obs", dist.Normal(theta, sigma), obs=y)
+    
 
 
 def eight_schools_noncentered_file(J, sigma, y=None):
-    #your code here
-    return
+    mu = pyro.sample("mu", dist.Normal(0., 5.))
+    tau = pyro.sample("tau", dist.HalfCauchy(5.))
+    with pyro.plate("schools", J):
+        nu = pyro.sample("nu", dist.Normal(0., 1.))
+        theta = mu + tau * nu
+        obs = pyro.sample("obs", dist.Normal(theta, sigma), obs=y)
